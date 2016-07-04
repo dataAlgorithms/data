252c43f0818ca724b9917c3c810c1bb3bd368e66
dataAlgorithms <195527444@qq.com>
2016-07-05 06:16:12 +0800

    add logger example

---
+++ scrapy/logger_example.py
@@ -0,0 +1,15 @@
+#! *-* coding=utf-8 *-*
+
+import scrapy 
+
+class MySpider(scrapy.Spider):
+    name = 'myspider'
+    allowed_domains = ['example.com']
+    start_urls = [
+        'http://www.example.com/1.html',
+        'http://www.example.com/2.html',
+        'http://www.example.com/3.html',
+    ]
+
+    def parse(self, response):
+        self.logger.info("A response from %s just arrived", response.url)
