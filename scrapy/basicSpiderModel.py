9ce2c1f81c512b53b00896db2d872c91c634f39a
dataAlgorithms <195527444@qq.com>
2016-07-07 07:01:48 +0800

    Update basicSpiderModel.py

--- scrapy/basicSpiderModel.py
+++ scrapy/basicSpiderModel.py
@@ -33,8 +33,8 @@ class MySpider(CrawlSpider):
         item['description'] = response.xpath('//td[@id="item_description"]/text()').extract()
         return item
         
-    2. csv spider model
-    from scrapy.spiders import CSVFeedSpider
+2. csv spider model
+from scrapy.spiders import CSVFeedSpider
 import scrapy 
 
 class MySpider(CSVFeedSpider):
@@ -114,3 +114,25 @@ class MySpider(SitemapSpider):
 
     def parse_other(self, response):
         pass
+
+4. xml spider
+#! *-* coding=utf-8 *-*
+
+from scrapy.spiders import XMLFeedSpider
+import scrapy 
+
+class MySpider(XMLFeedSpider):
+    name = 'example.com'
+    allowed_domains = ['example.com']
+    start_ursl = ['http://www.example.com/feed.xml']
+    iterator = 'iternodes'
+    itertag = 'item'
+
+    def parse_node(self, response, node):
+        self.logger.info('Hi, this is a <%s> node! %s', self.itertag, ''.join(node.extract()))
+
+        item = scrapy.Item()
+        item['id'] = node.xpath('@id').extract()
+        item['name'] = node.xpath('name').extract()
+        item['description'] = node.xpath('description').extract()
+        return item
