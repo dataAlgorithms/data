ba46f6cc4ff2c934646d24776bc3c742b074a3c8
dataAlgorithms <195527444@qq.com>
2016-07-08 06:57:43 +0800

    login spider

---
+++ scrapy/spiderLogin.py
@@ -0,0 +1,20 @@
+# Login in
+import scrapy
+
+class LoginSpider(scrapy.Spider):
+    name = 'example.com'
+    start_urls = ['http://www.example.com/users/login.php']
+
+    def parse(self, response):
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={'username':'john', 'password':'secret'},
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
+        if "authentication failed" in response.body:
+            self.logger.error("login failed")
+            return
+
+        # continue scraping with authenticated session
