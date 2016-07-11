08001cf43edb5f4a947ccb13c1dfff0c38314a39
dataAlgorithms <195527444@qq.com>
2016-07-12 05:49:43 +0800

    Create broadCrawl.py

---
+++ scrapy/broadCrawl.py
@@ -0,0 +1,27 @@
+#1. Increase concurrency
+CONCURRENT_REQUESTS = 100
+
+#2. Increase TwistedIO thread pool maximum size
+REACTOR_THREADPOOL_MAXSIZE = 20
+
+#3. Setup your own DNS
+# use OpenDNS
+
+#4. Reduce log level
+LOG_LEVEL = 'INFO'
+
+#5. Disable cookies
+COOKIES_ENABLED = False
+
+#6. Disable retries
+RETRY_ENABLED = False
+
+#7. Reduce download timeout
+DOWNLOAD_TIMEOUT = 15
+
+#8. Disable redirects
+REDIRECT_ENABLED = False
+
+#9. Enable crawling of Ajax pages
+AJAXCRAWL_ENABLED = True
+
