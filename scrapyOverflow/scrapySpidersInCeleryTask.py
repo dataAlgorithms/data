a490302ecce3e21d2103b351a6a61a88db16f029
dataAlgorithms <195527444@qq.com>
2016-10-31 19:12:07 +0800

    Update scrapySpidersInCeleryTask.py

--- scrapyOverflow/scrapySpidersInCeleryTask.py
+++ scrapyOverflow/scrapySpidersInCeleryTask.py
@@ -1,56 +1,27 @@
-'''
-
-accepted
-Okay here is how I got Scrapy working with my Django project that uses Celery for queuing up what to crawl. The actual workaround came primarily from joehillen's code located here http://snippets.scrapy.org/snippets/13/
-
-First the tasks.py file
-
-from celery import task
-
-@task()
-def crawl_domain(domain_pk):
-    from crawl import domain_crawl
-    return domain_crawl(domain_pk)
-Then the crawl.py file
-
-from multiprocessing import Process
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import Crawler
 from scrapy.conf import settings
-from spider import DomainSpider
-from models import Domain
+from myspider import MySpider
+from scrapy import log, project
+from twisted.internet import reactor
+from billiard import Process
+from scrapy.utils.project import get_project_settings
 
-class DomainCrawlerScript():
+class UrlCrawlerScript(Process):
+        def __init__(self, spider):
+            Process.__init__(self)
+            settings = get_project_settings()
+            self.crawler = Crawler(settings)
+            self.crawler.configure()
+            self.crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
+            self.spider = spider
 
-    def __init__(self):
-        self.crawler = CrawlerProcess(settings)
-        self.crawler.install()
-        self.crawler.configure()
+        def run(self):
+            self.crawler.crawl(self.spider)
+            self.crawler.start()
+            reactor.run()
 
-    def _crawl(self, domain_pk):
-        domain = Domain.objects.get(
-            pk = domain_pk,
-        )
-        urls = []
-        for page in domain.pages.all():
-            urls.append(page.url())
-        self.crawler.crawl(DomainSpider(urls))
-        self.crawler.start()
-        self.crawler.stop()
-
-    def crawl(self, domain_pk):
-        p = Process(target=self._crawl, args=[domain_pk])
-        p.start()
-        p.join()
-
-crawler = DomainCrawlerScript()
-
-def domain_crawl(domain_pk):
-    crawler.crawl(domain_pk)
-The trick here is the "from multiprocessing import Process" this gets around the "ReactorNotRestartable" issue in the Twisted framework. So basically the Celery task calls the "domain_crawl" function which reuses the "DomainCrawlerScript" object over and over to interface with your Scrapy spider. (I am aware that my example is a little redundant but I did do this for a reason in my setup with multiple versions of python [my django webserver is actually using python2.4 and my worker servers use python2.7])
-
-In my example here "DomainSpider" is just a modified Scrapy Spider that takes a list of urls in then sets them as the "start_urls".
-
-Hope this helps!
-
-http://stackoverflow.com/questions/11528739/running-scrapy-spiders-in-a-celery-task
-'''
+def run_spider(url):
+    spider = MySpider(url)
+    crawler = UrlCrawlerScript(spider)
+    crawler.start()
+    crawler.join()
