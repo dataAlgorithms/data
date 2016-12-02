e24b3f6cf8decbe3b737a314bfd77836d2670db9
dataAlgorithms <195527444@qq.com>
2016-12-02 19:22:03 +0800

    Update testConnection.py

--- spark/testConnection.py
+++ spark/testConnection.py
@@ -1,19 +1,32 @@
 import os
 import sys
  
-# Path for spark source folder
-os.environ['SPARK_HOME']="D:\Spark"
+def testConnection():
+    
+    # Path for spark source folder
+    os.environ['SPARK_HOME']="D:\Spark"
  
-# Append pyspark  to Python Path
-sys.path.append("D:\Spark\python")
-sys.path.append("D:\Spark\python\lib\py4j-0.10.3-src.zip")
+    # Append pyspark  to Python Path
+    sys.path.append("D:\Spark\python")
+    sys.path.append("D:\Spark\python\lib\py4j-0.10.3-src.zip")
  
-try:
-    from pyspark import SparkContext
-    from pyspark import SparkConf
+    try:
+        from pyspark import SparkContext
+        from pyspark import SparkConf 
  
-    print ("Successfully imported Spark Modules")
+        print ("Successfully imported Spark Modules")
  
-except ImportError as e:
-    print ("Can not import Spark Modules", e)
-    sys.exit(1)
+        sc = SparkContext(appName="TestApp")
+        lines = sc.textFile("d:\Spark\War_and_Peace.txt")
+        print lines.count()
+        print lines.first()
+        
+        return 0
+    
+    except ImportError as e:
+        print ("Can not import Spark Modules", e)
+        return 1
+    
+if __name__ == "__main__":
+    retCode = testConnection()
+    print 'retCode:', retCode
