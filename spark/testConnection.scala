6c788425107fe7ace02aa7d23e3c8fff9e6f2ce2
dataAlgorithms <195527444@qq.com>
2017-08-18 10:03:11 +0800

    Update testConnection.scala

--- spark/testConnection.scala
+++ spark/testConnection.scala
@@ -1,3 +1,6 @@
 import org.apache.spark._
 import SparkContext._
 val sc1 = new SparkContext()
+
+val config = new SparkConf().setMaster("spark://host:port").setAppName("big app")
+val sc2 = new SparkContext(config)
