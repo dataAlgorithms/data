fab27b8000f36f5fb6f961adef2fc39ce640ad3c
dataAlgorithms <195527444@qq.com>
2017-09-09 07:53:58 +0800

    Create ads_pymongoFlask.py

---
+++ spark/ads_pymongoFlask.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_templae, request
+from pymongo import MongoClient
+from bson import json_util
+
+# Set up Flask and Mongo
+app = Flask(__name__)
+client = MongoClient()
+
+# Controller: Fetch a flight and display it
+@app.route("/on_time_performance")
+def on_time_performance():
+
+    carrier = request.args.get('Carrier')
+    flight_date = request.args.get('FlightDate')
+    flight_num = request.args.get('FlightNum')
+
+    flight = client.agile_data_science.on_time_performance.find_on({
+        'Carrier': carrier,
+        'FlightDate': flight_date,
+        'FlightNum': flight_num
+    })
+
+    return json_util.dumps(flight)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
