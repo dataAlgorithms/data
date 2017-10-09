7c8a2da0b40c86b0016ea42c96c875ee5bcf8158
dataAlgorithms <195527444@qq.com>
2017-10-09 20:52:06 +0800

    Update iterGene_flattenNestedSeq.py

--- python/iterGene_flattenNestedSeq.py
+++ python/iterGene_flattenNestedSeq.py
@@ -21,3 +21,29 @@ for x in flatten(items):
 1 2 3 4 5 6 7 8
 
 Dave Paula Thomas Lewis
+
+In [3]: %paste
+from collections import Iterable
+
+def flatten(items, ignore_types=(str, bytes)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            for i in flatten(x):
+                yield i
+        else:
+            yield x
+
+items = [1, 2, [3, 4, [5, 6], 7], 8]
+for x in flatten(items):
+    print(x, end=' ')
+
+print("\r\n")
+
+items = ['Dave', 'Paula', ['Thomas', 'Lewis']]
+for x in flatten(items):
+    print(x, end=' ')
+## -- End pasted text --
+1 2 3 4 5 6 7 8
+
+Dave Paula Thomas Lewis
+In [4]:
