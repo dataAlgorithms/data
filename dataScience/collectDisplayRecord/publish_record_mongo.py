02147fcbf7586d6df2a58e52669f3a7058329ccc
dataAlgorithms <195527444@qq.com>
2017-10-11 06:17:41 +0800

    Update publish_record_mongo.py

--- dataScience/collectDisplayRecord/publish_record_mongo.py
+++ dataScience/collectDisplayRecord/publish_record_mongo.py
@@ -16,3 +16,20 @@ on_time_dataframe = spark.read.parquet('data/on_time_performance.parquet')
 
 as_dict = on_time_dataframe.rdd.map(lambda row: row.asDict())
 as_dict.saveToMongoDB('mongodb://localhost:27017/agile_data_science.on_time_performance')
+
+'''
+#if something goes wrong, drop the collection
+
+mongo agile_data_science
+> db.on_time_performance.drop()
+
+#do the above program, then query
+
+> db.on_time_performance.findOne()
+or
+> db.on_time_performance.findOne(
+{Carrier: 'DL', FlightDate: '2015-01-01', FlightNum: 478}) 
+
+# maintain indexes for query to make query return quickly
+> db.on_time_performance.ensureIndex({Carrier: 1, FlightDate: 1, FlightNum: 1})
+'''
