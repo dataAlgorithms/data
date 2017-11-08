9ba4b2b9aa136c4d6531733c478d18616d450cb5
dataAlgorithms <195527444@qq.com>
2017-11-09 06:25:05 +0800

    Create visualize_exploreSeasonality.py

---
+++ dataScience/agileDataScience/visualize_exploreSeasonality.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from bson import json_util
+
+# Set up Flask and Mongo
+app = Flask(__name__)
+client = MongoClient()
+
+# Controller: Fetch a flight chart
+@app.route("/total_flights")
+def total_flights():
+    total_flights = client.agile_data_science.flights_by_month.find({},
+       sort = [
+         ('Year', 1),
+         ('Month', 1)
+       ])
+    return render_template('total_flights.html', total_flights=total_flights)
+
+# Controller: Fetch a flight chart
+@app.route("/total_flights_chart")
+def total_flights_chart():
+    total_flights = client.agile_data_science.flights_by_month.find({},
+      sort = [
+        ('Year', 1),
+        ('Month', 1)
+      ])
+    return render_template('total_flights_chart.html', total_flights=total_flights)
+
+@app.route("/total_flights.json")
+def total_flights_json():
+    total_flights = client.agile_data_science.flights_by_month.find({},
+      sort = [
+       ('Year', 1),
+       ('Month', 1)
+      ])
+    return json_util.dumps(total_flights, ensure_ascii=False)
+
+# Controller: Fetch a flight and display it
+@app.route("/airplane/flights/<tail_number>")
+def flights_per_airplane(tail_number):
+    flights = client.agile_data_science.flights_per_airplane.find_one({'TailNum': tail_number})
+    return render_template('flights_per_airplane.html', flights=flights, tail_number=tail_number)
+
+if __name__ == "__main__":
+  app.run(debug=True, port=5002)
+  
+'''
+http://localhost:5002/total_flights_chart
+
+
+'''
