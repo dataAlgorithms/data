fd861ad7c8213fb719c480b499a803bd2e52e7bd
dataAlgorithms <195527444@qq.com>
2017-12-11 08:47:16 +0800

    Create function_returnMultipleValueFromFunction.py

---
+++ python/function_returnMultipleValueFromFunction.py
@@ -0,0 +1,19 @@
+In [4]: %paste
+def myfun():
+    return 1, 2, 3
+
+a, b, c = myfun()
+a
+b
+c
+## -- End pasted text --
+Out[4]: 3
+
+In [5]: a
+Out[5]: 1
+
+In [6]: b
+Out[6]: 2
+
+In [7]: c
+Out[7]: 3
